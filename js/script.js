7b57ab9bbd871447b6700b6b0b070ac148b0dd4c
Sahedul Islam Rony <sahed.cse.just@gmail.com>
2024-09-06 15:09:17 +0600

    Ready for production build

--- js/script.js
+++ js/script.js
@@ -41,9 +41,6 @@ function changeSlide(index) {
     // reset previous slide
     slides.forEach((slide) => {
       slide.classList.remove("top");
-    });
-
-    slides.forEach((slide) => {
       slide.classList.remove("next");
     });
 
@@ -67,7 +64,7 @@ function changeButton(index) {
   buttons[index].classList.add("active");
 }
 
-// automatic slide change effet
+// automatic slide change effect
 
 setInterval(() => {
   let currentIndex = Array.from(slides).findIndex((slide) =>
@@ -78,3 +75,39 @@ setInterval(() => {
 
   changeSlide(nextIndex);
 }, 4000);
+
+// Populate links
+
+const fb = document.querySelectorAll(".fb");
+const twitter = document.querySelectorAll(".twitter");
+const lin = document.querySelectorAll(".linkedin");
+const email = document.querySelectorAll(".email");
+
+fb.forEach((link) => {
+  link.setAttribute("href", "https://www.facebook.com/sahedulislamFB");
+  link.target = "_blank";
+});
+twitter.forEach((link) => {
+  link.setAttribute("href", "https://www.x.com/i_am_sahed");
+  link.target = "_blank";
+});
+lin.forEach((link) => {
+  link.setAttribute("href", "https://www.linkedin.com/in/sahedulislamrony");
+  link.target = "_blank";
+});
+email.forEach((link) => {
+  link.setAttribute("href", "mailto:cast.sahedul@gmail.com");
+});
+
+const links = document.querySelectorAll("a");
+
+// reset default behaviour of empty links
+links.forEach((link) => {
+  let href = link.getAttribute("href");
+
+  if (href === "#" || !href) {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+  }
+});
